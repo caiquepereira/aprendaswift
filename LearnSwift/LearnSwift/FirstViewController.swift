905731a7521e99da7cef6ad4d0d1f1f236a2bf61
Caique Pereira <caiquepereira@gmail.com>
2018-05-31 20:52:08 -0300

    Recreated the project in Swift

---
+++ LearnSwift/LearnSwift/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  LearnSwift
+//
+//  Created by Caique Pereira on 31/05/18.
+//  Copyright © 2018 Caique Pereira. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ LearnSwift/LearnSwift/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LearnSwift
+//
+//  Created by Caique Pereira on 31/05/18.
+//  Copyright © 2018 Caique Pereira. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
